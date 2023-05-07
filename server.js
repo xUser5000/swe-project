fbf609ce16c61e7bcd1bc977a5b023ab8d5f7953
Your Name <you@example.com>
2023-05-08 00:13:56 +0300

    task

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ import express from "express";
 import { engine } from "express-handlebars";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-dotenv.config {};
+dotenv.config();
 
 
-mongoose.connect(pross.env.mongoconnectionurl);
+mongoose.connect(process.env.mongoConnectionUrl);
 
 const app = express();
 
@@ -15,6 +15,9 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './templates');
 
+app.get('/', (req, res) => {
+    res.render('departments/all');
+})
 app.listen(process.env.port, () => (
-    console.log(`started the application on http;//localhost:${process.env.port}`)
+    console.log(`started the application on http://localhost:${process.env.port}`)
 ));
